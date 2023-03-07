2be8d44fa941be157553ec3b74d225f6ebaae3d0
Diego Miszczuk <diegohmiszczuk@gmail.com>
2023-03-07 09:02:20 -0300

    Agrego bootstrap

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Mzk Shop</h1>
+            <div>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Navbar
