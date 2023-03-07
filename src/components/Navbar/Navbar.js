3924c3417d905758edd367853ce486d9591982ea
Diego Miszczuk <diegohmiszczuk@gmail.com>
2023-03-07 11:13:26 -0300

    agrego biblioteca de iconos de bootstrap

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,31 @@
+import "./Navbar.css"
+
+import '../../../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+
+
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="Navbar">
             <h1>Mzk Shop</h1>
-            <div>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
+            {/* <div>
+                <button className="btn btn-primary">1</button>
+                <button className="btn btn-primary">2</button>
+                <button className="btn btn-primary">3</button>
+            </div> */}
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-primary">Left</button>
+                <button type="button" class="btn btn-primary">Middle</button>
+                <button type="button" class="btn btn-primary">Right</button>
             </div>
-            
+            <div>
+            <button type="button" class="btn btn-primary"><i className="bi bi-cart"></i>
+                Carrito</button>
+                
+            </div>
         </nav>
+
+
+        
     )
 }
 
