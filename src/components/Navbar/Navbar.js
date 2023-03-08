b73922a53f48d523a905d6c7b46d8742a38c00da
Diego Miszczuk <diegohmiszczuk@gmail.com>
2023-03-07 22:05:02 -0300

    agrego img fondo navbar y color de texto

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,16 +6,16 @@ import '../../../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 const Navbar = () => {
     return (
         <nav className="Navbar">
-            <h1>Mzk Shop</h1>
+            <h1 className="text-blanco">Mzk Shop</h1>
             {/* <div>
                 <button className="btn btn-primary">1</button>
                 <button className="btn btn-primary">2</button>
                 <button className="btn btn-primary">3</button>
             </div> */}
             <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-primary">Left</button>
-                <button type="button" class="btn btn-primary">Middle</button>
-                <button type="button" class="btn btn-primary">Right</button>
+                <button type="button" class="btn btn-primary"> CNC Wood Carving</button>
+                <button type="button" class="btn btn-primary">3d Prints</button>
+                <button type="button" class="btn btn-primary">Supplies</button>
             </div>
             <div>
             <button type="button" class="btn btn-primary"><i className="bi bi-cart"></i>
