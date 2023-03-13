093e3e0f16b246fad98a154361a7e12d0fcd514a
Diego Miszczuk <diegohmiszczuk@gmail.com>
2023-03-13 10:20:56 -0300

    formateo Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 import "./Navbar.css"
-=======
 import './Navbar.css'
 import '../../general.css'
->>>>>>> preentrega1-miszczuk
 import '../../../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import CartWidget from "../CartWidget/CartWidget";
 import logo from './assets/logo.png'
@@ -12,26 +10,15 @@ const Navbar = () => {
     return (
         <nav className="Navbar">
             <div className='namePosition'>
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" />
                 <h1 className="textColorPpal">Mzk Shop</h1>
             </div>
-            
-            
-
             <div class="btn-group" role="group" aria-label="Basic example">
                 <button type="button" class="btn btn-outline-light"> CNC Wood Carving</button>
                 <button type="button" class="btn btn-outline-light">3d Prints</button>
                 <button type="button" class="btn btn-outline-light">Supplies</button>
             </div>
-<<<<<<< HEAD
-            {/* <div>
-            <button type="button" class="btn btn-outline-light"><i className="bi bi-cart"></i>
-                Carrito</button>
-                
-            </div> */}
-=======
             <CartWidget />
->>>>>>> preentrega1-miszczuk
         </nav>
     )
 }
