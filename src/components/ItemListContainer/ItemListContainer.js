44cc67447634b81ce2ebffc92db0e02813bddddb
Diego Miszczuk <diegohmiszczuk@gmail.com>
2023-03-22 08:36:57 -0300

    agrego productos a itemListContainer

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react'
+import { getProducts } from "../../asyncMock"
+
+
+
 const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        getProducts()
+            .then(products => {
+                setProducts(products)
+            })
+    }, [])
+    console.log(products)
+
+
+
     return (
         <div>
             <h1 className="textColorSec">{greeting}</h1>
+            <div>
+                {products.map(products => {
+                    return (
+                        <div key={products.id}>
+                            <h3>{products.name}</h3>
+                            <img src={products.img} alt={products.name}></img>
+                            <p>Precio: ${products.price}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
